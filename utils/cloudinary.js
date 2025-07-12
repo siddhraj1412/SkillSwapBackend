063fe66777566bfc30b4ec85be518f84c0064c74
siddhraj1412 <siddhrajthakor2304@gmail.com>
2025-07-12 10:52:36 +0530

    Cloudinary image config

---
+++ utils/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'skill_swap_profiles',
+    allowed_formats: ['jpg', 'png'],
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { upload, cloudinary };
