377b0d1ef12066044e845b5d0b5d87d1ae0582b5
siddhraj1412 <siddhrajthakor2304@gmail.com>
2025-07-12 10:54:11 +0530

    updated auth.routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../controllers/auth.controller');
+const { signup, login, forgotPassword, verifyOTP, resetPassword } = require('../controllers/auth.controller');
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-otp', verifyOTP);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
